d8bd271dfc9554ce85dcdf26e13115dcc59e9710
Tan Chee Liang <kenntcl@gmail.com>
2015-03-07 19:42:20 +0800

    Updates to UI.R

--- UI.R
+++ UI.R
@@ -6,9 +6,9 @@ shinyUI(pageWithSidebar(
                 p('Enter the gross horsepower, number of cylinders, and weight of your
                   car in the fields below. The predicted MPG will be shown on your right.'),
                 h3('Please enter the predictors of MPG below.'),
-                numericInput('hp', 'Gross horsepower:', 140, min = 50, max = 330, step = 10), # example of numeric input
+                numericInput('hp', 'Gross horsepower:', 100, min = 50, max = 350, step = 10), # example of numeric input
                 radioButtons('cyl', 'Number of cylinders:', c('4' = 4, '6' = 6, '8' = 8), selected = '4'), # example of radio button input
-                numericInput('wt', 'Weight (lbs):', 3200, min = 1500, max = 5500, step = 100)
+                numericInput('wt', 'Weight (lbs):', 3500, min = 1000, max = 5500, step = 100)
                 ),
         mainPanel(
                 h3('Predicted MPG'),
@@ -18,6 +18,11 @@ shinyUI(pageWithSidebar(
                 verbatimTextOutput("prediction"),
                 plotOutput('plots'),
                 p(),
-                h5('Note: The data was extracted from the 1974 Motor Trend US magazine, and comprises fuel consumption and 10 aspects of automobile design and performance for 32 automobiles (1973-74 models).')
+                h5('Note: The data was extracted from the 1974 Motor Trend US magazine, and comprises fuel consumption and 10 aspects of automobile design and performance for 32 automobiles (1973-74 models).'),
+                p(),
+                h3('How to use the application:'),
+                p(),
+                h5('To use the application, simply enter the gross horsepower, number of cylinders and weight of your car in the fields provided on the side bar panel.'),
+                h5('The result will be shown automatically on the main panel.')
         )
 ))
